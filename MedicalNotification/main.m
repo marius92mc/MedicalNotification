53b73478515f5b3a29509301efd3dc9d27977282
Andrei Constantinescu <andrei.c@live.com>
2014-03-11 18:23:34 +0200

    Initial Commit

---
+++ MedicalNotification/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MedicalNotification
+//
+//  Created by Andrei Constantinescu on 11/03/14.
+//  Copyright (c) 2014 Andrei Constantinescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
